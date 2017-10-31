b3d9a8a227781ba4a41ca5bc29c7c0a9be3d5e7e
Akshay <akshay365@icloud.com>
2017-10-31 18:04:04 -0400

    1969 Project

---
+++ js/script.js
@@ -0,0 +1,25 @@
+var $filterableRows = $('#MI6').find('tr').not(':first'),
+		$inputs = $('.search-key');
+
+$inputs.on('input', function() {
+
+	$filterableRows.hide().filter(function() {
+  	return $(this).find('td').filter(function() {
+    	
+      var tdText = $(this).text().toLowerCase(),
+      		inputValue = $('#' + $(this).data('input')).val().toLowerCase();
+    
+    	return tdText.indexOf(inputValue) != -1;
+    
+    }).length == $(this).find('td').length;
+  }).show();
+
+});
+
+
+function toggle(source) {
+  checkboxes = document.getElementsByName('foo');
+  for(var i=0, n=checkboxes.length;i<n;i++) {
+    checkboxes[i].checked = source.checked;
+  }
+}
---
+++ js/tableJS.js
@@ -0,0 +1,64 @@
+function myFunction() {
+      // Declare variables 
+      var input, filter, table, tr, td, i, occurrence;
+
+      input = document.getElementById("myInput");
+      filter = input.value.toUpperCase();
+      table = document.getElementById("myTable");
+      tr = table.getElementsByTagName("tr");
+
+      // Loop through all table rows, and hide those who don't match the search query
+     for (i = 0; i < tr.length; i++) {
+         occurrence = false; // Only reset to false once per row.
+         td = tr[i].getElementsByTagName("td");
+         for(var j=0; j< td.length; j++){                
+             currentTd = td[j];
+             if (currentTd ) {
+                 if (currentTd.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                     tr[i].style.display = "";
+                     occurrence = true;
+                 } 
+             }
+         }
+         if(!occurrence){
+             tr[i].style.display = "none";
+         } 
+     }
+   }
+
+
+function toggle(source) {
+  checkboxes = document.getElementsByName('foo');
+  for(var i=0, n=checkboxes.length;i<n;i++) {
+    checkboxes[i].checked = source.checked;
+  }
+}
+
+
+function myFunction2() {
+  // Declare variables
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("myInput2");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+function toggle(source) {
+  checkboxes = document.getElementsByName('foo');
+  for(var i=0, n=checkboxes.length;i<n;i++) {
+    checkboxes[i].checked = source.checked;
+  }
+}
